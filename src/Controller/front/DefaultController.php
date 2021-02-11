a3307c505e2e29584958432ae03cf20528afca16
AntoineCerqueus <70634327+AntoineCerqueus@users.noreply.github.com>
2021-02-11 11:37:44 +0100

    footer done with cdv & legal mentions pages

--- src/Controller/front/DefaultController.php
+++ src/Controller/front/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class DefaultController extends AbstractController
 {
     /**
@@ -13,8 +14,27 @@ class DefaultController extends AbstractController
      */
     public function home(): Response
     {
-        return $this->render('front/default/home.html.twig', [
-            'controller_name' => 'Tonio',
-        ]);
+        return $this->render('front/default/home.html.twig'
+        // , [
+        //     'controller_name' => 'Tonio',
+        // ]
+    );
     }
+
+    /**
+     * @Route("/cgv", name="cgv")
+     */
+    public function cgv(): Response
+    {
+        return $this->render('front/default/cgv.html.twig');
+    }
+
+    /**
+     * @Route("/mentions-légales", name="legal_mentions")
+     */
+    public function legalMentions(): Response
+    {
+        return $this->render('front/default/legal-mentions.html.twig');
+    }
+
 }
