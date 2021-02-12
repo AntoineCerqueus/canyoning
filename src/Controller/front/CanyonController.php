ff3d082c6652b99396f17544ed4e1c72daa21412
AntoineCerqueus <70634327+AntoineCerqueus@users.noreply.github.com>
2021-02-12 16:23:18 +0100

    front/canyon

--- src/Controller/front/CanyonController.php
+++ src/Controller/front/CanyonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\front;
 
+use App\Entity\Canyon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,22 +14,24 @@ use App\Repository\CanyonRepository;
 class CanyonController extends AbstractController
 {
     /**
-     * @Route("/canyon", name="canyon")
-     */
-    // public function index(): Response
-    // {
-    //     return $this->render('canyon/index.html.twig', [
-    //         'controller_name' => 'CanyonController',
-    //     ]);
-    // }
-
-    /**
      * @Route("/index", name="index", methods={"GET"})
      */
     public function index(CanyonRepository $canyonRepository): Response
     {
+        // Récupération de tout les canyons dans le but de les afficher avec une brève présentation
         return $this->render('front/canyon/index.html.twig', [
             'canyons' => $canyonRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/single/{id}", name="single", methods={"GET"})
+     */
+    public function single(Canyon $canyon):Response
+    {
+        // Permets d'afficher le canyon dans son intégralité en récuprant son id grâce à le boucle du 'findAll'
+        return $this->render('front/canyon/single.html.twig',[
+            'canyon' => $canyon
         ]);
     }
 }
